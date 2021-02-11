ed4e0436df67f0ce6be960ded007b14f8044d9f2
Leon Kielstra <leon.kielstra@nedap.com>
2021-02-11 16:05:02 +0100

    Add python flask server

---
+++ python-flask-server/swagger_server/controllers/default_controller.py
@@ -0,0 +1,47 @@
+import connexion
+import six
+
+from swagger_server.models.student import Student  # noqa: E501
+from swagger_server import util
+from swagger_server.services import student_service
+
+
+def add_student(body):  # noqa: E501
+    """Add a new student
+
+     # noqa: E501
+
+    :param body: Student object that needs to be added
+    :type body: dict | bytes
+
+    :rtype: int
+    """
+    if connexion.request.is_json:
+        body = Student.from_dict(connexion.request.get_json())  # noqa: E501
+    return student_service.add_student(body)
+
+
+def delete_student(student_id):  # noqa: E501
+    """Delete student by ID
+
+     # noqa: E501
+
+    :param student_id: ID of student to return
+    :type student_id: int
+
+    :rtype: Student
+    """
+    return student_service.delete_student(student_id)
+
+
+def get_student_by_id(student_id):  # noqa: E501
+    """Find student by ID
+
+    Returns a single student # noqa: E501
+
+    :param student_id: ID of student to return
+    :type student_id: int
+
+    :rtype: Student
+    """
+    return student_service.get_student_by_id(student_id, None)
